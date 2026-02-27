c1e673f0a6d119e54151e794afd90630e3fa930c
Tali <you@example.com>
2026-02-27 18:14:23 +0200

    Stop tracking dist folder

--- dist/controllers/commentsController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.commentsController = void 0;
-const commentModel_1 = require("../model/commentModel");
-const baseController_1 = require("./baseController");
-exports.commentsController = new baseController_1.BaseController(commentModel_1.Comment);
-//# sourceMappingURL=commentsController.js.map
--- dist/controllers/moviesController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.moviesController = void 0;
-const movieModel_1 = require("../model/movieModel");
-const baseController_1 = require("./baseController");
-exports.moviesController = new baseController_1.BaseController(movieModel_1.Movie);
-//# sourceMappingURL=moviesController.js.map
